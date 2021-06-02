239f497da5e6148b76ce2d34ed000b61692a5a5a
Hieu <nguyenthanhhieuptit@gmail.com>
2021-06-02 22:30:16 +0700

    update

--- src/main/java/application/service/FileUploadHelper.java
+++ src/main/java/application/service/FileUploadHelper.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUploadHelper {
 	
 	public final String UPLOAD_DIR=new ClassPathResource("/static/image/").getFile().getAbsolutePath();
-
+	
 	public FileUploadHelper() throws IOException {
 	}
 
